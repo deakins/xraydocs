c96050715b7ca3ecead6d38e09ca7b8c96941ad1
Daniel Eakins <daniel.eakins@eng.ox.ac.uk>
2019-07-24 21:39:08 +0100

    Added command to automatically upload all files to web server after committing changes.

--- test.sh
+++ test.sh
@@ -2,3 +2,4 @@ source /Users/deakins/.bash_profile
 source /Library/Frameworks/Python.framework/Versions/3.6/bin/virtualenvwrapper.sh
 workon xrenv
 make html
+scp -r _build/html/* engs1751@linux.ox.ac.uk:public_html/wiki/
