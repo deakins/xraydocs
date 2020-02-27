c11246706380709d9b9dfe1d728719c2d10840e6
Daniel Eakins <daniel.eakins@eng.ox.ac.uk>
2020-02-27 12:39:41 +0000

    Corrected buildpost.sh after testing in terminal.

--- buildpost.sh
+++ buildpost.sh
@@ -3,4 +3,4 @@ source /Library/Frameworks/Python.framework/Versions/3.6/bin/virtualenvwrapper.s
 workon sphinx3
 make html
 rsync -arvhP -e ssh _build/html/* engs1751@linux.ox.ac.uk:public_html/wiki/
-rsync -arvhP -e ssh _build/html/* DanE@163.1.15.221:share/Web/Wiki/
+rsync -arvhP -e "ssh -p 22" _build/html/* DanE@163.1.15.221:/share/Web/Wiki
