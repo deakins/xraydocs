c52fdd1b2796aae6cc45a33ffce848f14b107265
Daniel Eakins <daniel.eakins@eng.ox.ac.uk>
2020-03-18 19:53:33 +0000

    Corrected the IP address of the QNAP NAS.

--- buildpost.sh
+++ buildpost.sh
@@ -2,4 +2,4 @@ source /Users/deakins/.bash_profile
 source /Library/Frameworks/Python.framework/Versions/3.6/bin/virtualenvwrapper.sh
 workon sphinx3
 make html
-rsync -arvhP -e "ssh -p 22" _build/html/* DanE@163.1.15.221:/share/Web/Wiki
+rsync -arvhP -e "ssh -p 22" _build/html/* DanE@163.1.14.53:/share/Web/Wiki
